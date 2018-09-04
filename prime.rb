22327926265a43ad1eefa6b8efcb87d67a2c777a
Ashley Wong <ashwong04@gmail.com>
2018-09-04 16:05:27 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,8 @@
 # Add  code here!
 def prime?(num)
   return false if num < 2
-    (2..integer - 1).each do |x|
-        if (integer % x) == 0
+    (2..num - 1).each do |x|
+        if (num % x) == 0
             return false
         end
     end
